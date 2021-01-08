e51b210e167104f3bf64fd7c2221dbb9b3980631
yusuke0208na@gmail.com <ec2-user@ip-172-31-84-168.ec2.internal>
2021-01-08 11:35:32 +0000

    [Start] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '70fa91d8828719fb3bb1dc5d15eba6b5f33d586291d70b3ba62f6f7edbc2247079be543af6a7f4b203ed5d3b8781c46608da190a58885b36d088682b3a12a503'
